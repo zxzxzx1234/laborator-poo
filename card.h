e351906143b3ba4d175d4ee3d11404edcaaeb733
Stefan <stefan.popa165@gmail.com>
2025-05-19 16:28:25 +0300

    Update card.h

--- card.h
+++ card.h
@@ -1,14 +1,16 @@
 #ifndef CARD_H
 #define CARD_H
 #include <SFML/Graphics.hpp>
+#include "igame_object.h"
 
 using namespace std;
-// class for a single card
-class Card
+
+// Class for a single card
+class Card : public IGameObject // Inherit from IGameObject
 {
 private:
-    string m_suit; //suit for the card
-    int m_rank; // rank for the card
+    string m_suit; // Suit for the card
+    int m_rank;    // Rank for the card
     string _GetCardHash();
     sf::Sprite m_card;
 
@@ -17,8 +19,9 @@ public:
     Card(string suit, int rank);
     ~Card();
     int GetCardRank() { return this->m_rank; }
-    void DrawTo(sf::RenderWindow& window);
+    void DrawTo(sf::RenderWindow& window) override; // Implement the IGameObject method
     void SetCardPosition(sf::Vector2f pos);
+    void SetColor(sf::Color color);
 };
 
 #endif
