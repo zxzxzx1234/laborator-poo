dbc6142b94cc51df7ba62313244b2a48f59b5e51
Stefan <stefan.popa165@gmail.com>
2025-05-26 15:00:44 +0300

    Update vegas_lights.cpp

--- vegas_lights.cpp
+++ vegas_lights.cpp
@@ -11,21 +11,23 @@ namespace VegasLights {
         auto& lights = GetLights();
         lights.clear();
 
-        float base_y = 700.f; 
-        float spacing = 180.f; 
-        float start_x = 150.f;
+        float base_y = 540.f;  
+        float spacing = 90.f;    
+        float start_x = 120.f;       
 
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 2; ++i) {
             Light l;
-            l.base_radius = 35 + i * 5;
+            l.base_radius = 20 + i * 2;
             l.phase = i * 1.57f;
             l.shape.setRadius(l.base_radius);
-            l.shape.setFillColor(sf::Color(255, 255, 0, 80)); 
+            l.shape.setFillColor(sf::Color(255, 255, 100, 90));
             l.shape.setOrigin(l.base_radius, l.base_radius);
-            l.shape.setPosition(start_x + i * spacing, base_y);  
+            l.shape.setPosition(start_x + i * spacing, base_y); 
             lights.push_back(l);
         }
     }
+
+
 
 
     void Update(float time) {
